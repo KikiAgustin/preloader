01699cbbe6de6f3ac79c059ffe09d49470351663
davidaja <davidabdul365@gmail.com>
2022-03-05 11:10:52 +0800

    upload pertama

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// window.onload = function (){
+//     window.setTimeout(fadeOut, 500);
+// }
+
+// function fadeOut(){
+//     document.querySelector('.preloader').style.opacity = '0';
+//     document.querySelector('preloader').style.display = 'none';
+// }
