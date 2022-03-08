c9b5f8eb6ab32de4e5f4181a60f7ef807c3f61b8
kikiAgustin <kikiagustin62@gmail.com>
2022-03-08 22:51:59 +0600

    edit file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// window.onload = function (){
-//     window.setTimeout(fadeOut, 500);
-// }
+window.onload = function () {
+    window.setTimeout(fadeOut, 1000);
+}
 
-// function fadeOut(){
-//     document.querySelector('.preloader').style.opacity = '0';
-//     document.querySelector('preloader').style.display = 'none';
-// }
+function fadeOut() {
+    document.querySelector('.preloader').style.opacity = '0';
+    document.querySelector('preloader').style.display = 'none';
+}
